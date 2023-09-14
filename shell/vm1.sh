a78e007ad6924b1729a39e741be4f0c845a7995a
Adauto Turíbio <adautoturibio73@gmail.com>
2023-09-14 20:16:23 -0300

    finalizando o funcionamento do gateway

--- shell/vm1.sh
+++ shell/vm1.sh
@@ -2,5 +2,7 @@
 #sudo apt update #listar repositórios de atualização
 #sudo apt upgrade # instalar atualizações
 echo "route add default gw 192.168.50.12" >> /etc/network/interfaces # configurando a vm3 como gateway padrão
+sudo ip route add default via 192.168.50.12 #defininfo o ip da terceira vm como rota padrão
+sudo iptables -t nat -A POSTROUTING -o enp0s3 -j MASQUERADE # configurando o NAT para traduzir e rotear corretamente
 sudo apt-get install apache2 -y # instalar servidor apache
 sudo mkdir -p /var/www/html # criar diretório do servidor web
--- shell/vm2.sh
+++ shell/vm2.sh
@@ -2,4 +2,6 @@
 #sudo apt update #listar repositórios de atualização
 #sudo apt upgrade # instalar atualizações
 echo "route add default gw 192.168.50.12" >> /etc/network/interfaces # configurando a vm3 como gateway padrão
+sudo ip route add default via 192.168.50.12 #defininfo o ip da terceira vm como rota padrão
+sudo iptables -t nat -A POSTROUTING -o epn0s3 -j MASQUERADE # configurando o NAT para traduzir e rotear corretamente
 sudo apt-get install mysql-server -y # instalar servidor mysql
