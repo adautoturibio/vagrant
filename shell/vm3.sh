fa7debd8d87651225f0bbf8d0b63f0d68b4e11de
Adauto Turíbio <adautoturibio73@gmail.com>
2023-09-14 22:15:54 -0300

    correções no gateway

--- shell/vm3.sh
+++ shell/vm3.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#sudo apt update #listar repositórios de atualização
-#sudo apt upgrade # instalar atualizações
+sudo apt update #listar repositórios de atualização
+sudo apt upgrade # instalar atualizações
 echo 'net.ipv4.ip_forward=1' >> /etc/sysctl.conf && sysctl -p # habilitar o redirecionamento de pacotes no kernel da VM do gateway
-iptables -t nat -A POSTROUTING -o eth1 -j MASQUERADE # encaminhar o tráfego da rede externa para a Internet.
+iptables -t nat -A POSTROUTING -o enp0s3 -j MASQUERADE # encaminhar o tráfego da rede externa para a Internet.
