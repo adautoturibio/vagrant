3411b55537696e0f70e7ffb85b05ceb8093ae9bd
Adauto Turíbio <adautoturibio73@gmail.com>
2023-09-14 22:23:20 -0300

    adicionando variável de ambiente

--- shell/vm2.sh
+++ shell/vm2.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+export INTERFACE="enp0s3"
 sudo apt update #listar repositórios de atualização
 sudo apt upgrade # instalar atualizações
 sudo apt-get install mysql-server -y # instalar servidor mysql
 echo "route add default gw 192.168.50.12" >> /etc/network/interfaces # configurando a vm3 como gateway padrão
 sudo ip route add default via 192.168.50.12 #defininfo o ip da terceira vm como rota padrão
-sudo iptables -t nat -A POSTROUTING -o epn0s3 -j MASQUERADE # configurando o NAT para traduzir e rotear corretamente
+sudo iptables -t nat -A POSTROUTING -o $INTERFACE -j MASQUERADE # configurando o NAT para traduzir e rotear corretamente
--- shell/vm3.sh
+++ shell/vm3.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-sudo apt update #listar repositórios de atualização
-sudo apt upgrade # instalar atualizações
+export INTERFACE="enp0s3"
+#sudo apt update #listar repositórios de atualização
+#sudo apt upgrade -y # instalar atualizações
 echo 'net.ipv4.ip_forward=1' >> /etc/sysctl.conf && sysctl -p # habilitar o redirecionamento de pacotes no kernel da VM do gateway
-iptables -t nat -A POSTROUTING -o enp0s3 -j MASQUERADE # encaminhar o tráfego da rede externa para a Internet.
+iptables -t nat -A POSTROUTING -o $INTERFACE -j MASQUERADE # encaminhar o tráfego da rede externa para a Internet.
