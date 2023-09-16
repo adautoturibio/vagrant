9aa37690b1f34914a5b22cbfebf6ce74efed4aec
Adauto Turíbio <adautoturibio73@gmail.com>
2023-09-15 22:29:06 -0300

    variavel de ambiente na vm3

--- shell/vm3.sh
+++ shell/vm3.sh
@@ -3,4 +3,4 @@ export INTERFACE="enp0s3"
 #sudo apt update #listar repositórios de atualização
 #sudo apt upgrade -y # instalar atualizações
 echo 'net.ipv4.ip_forward=1' >> /etc/sysctl.conf && sysctl -p # habilitar o redirecionamento de pacotes no kernel da VM do gateway
-iptables -t nat -A POSTROUTING -o  -j MASQUERADE # encaminhar o tráfego da rede externa para a Internet.
+iptables -t nat -A POSTROUTING -o $INTERFACE -j MASQUERADE # encaminhar o tráfego da rede externa para a Internet.
